57dbe44c8c865161eea71d81647364a6adcf1d8d
Swarnim55 <72058504+Swarnim55@users.noreply.github.com>
2022-04-18 09:00:29 +0545

    MERN-Ecommerce-Created Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">E-Store</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
